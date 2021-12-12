479a2fd998d0553ecc9a623070d618f90060168d
magmom95 <magmom95@naver.com>
2021-12-13 01:14:43 +0900

    test useState

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,16 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  let posts = '연습중이에여';
+  // let posts = '연습중이에여';
+
+  let [글제목, 글제목변경] = useState(['유튜브 추천', '룰루랄라','라랄루룰']);
 
   return (
     <div className="App">
       <div className="color-nav">
         <div className="main" style={{ color : 'black', fontSize : '30px'}}>연습용 Blog</div>
       </div>
-      <img src={ logo } alt="sdf"/>
-      <h4>{ posts }</h4>
+      <div className='list'>
+        {/* <h4>{ posts }</h4> */}
+        <h3>{글제목[0]}</h3>
+        <p>12월 12일 </p>
+        <hr/>
+      </div>
+      <div className='list'>
+        {/* <h4>{ posts }</h4> */}
+        <h3>{글제목[1]}</h3>
+        <p>12월 12일 </p>
+        <hr/>
+      </div>
+      <div className='list'>
+        {/* <h4>{ posts }</h4> */}
+        <h3>{글제목[2]}</h3>
+        <p>12월 12일 </p>
+        <hr/>
+      </div>
     </div>
   );
 }
