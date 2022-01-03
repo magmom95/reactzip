e7a6fe3b707e0512810e9bc45705bf8756cfd36d
magmom95 <magmom95@naver.com>
2022-01-04 01:53:05 +0900

    Props shoes find index Item

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -5,6 +5,9 @@ function Detail(props) {
 
     let history = useHistory();
     let { id } = useParams();
+    let finditem = props.shoes.find(function(item){
+        return item.id == id
+    });
 
 ;    return(
         <div className="container">
@@ -13,9 +16,9 @@ function Detail(props) {
             <img src="https://github.com/magmom95/interex/blob/main/nike2.JPG?raw=true" width="100%" />
           </div>
           <div className="col-md-6 mt-4">
-            <h4 className="pt-5">{props.shoes[id].title}</h4>
-              <p>{props.shoes[id].content}</p>
-              <p>{props.shoes[id].price}원</p>
+            <h4 className="pt-5">{finditem.title}</h4>
+              <p>{finditem.content}</p>
+              <p>{finditem.price}원</p>
             <button className="btn btn-danger">주문하기</button>
             <br/><br/>  
             <button className="btn btn-warning" onClick={() =>{ 
