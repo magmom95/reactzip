53e85d1c0ae366c1f33faa0198be0589ccc67df1
magmom95 <magmom95@naver.com>
2022-01-14 21:43:43 +0900

    Add CSSTransition

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -3,6 +3,8 @@ import { useHistory, useParams } from 'react-router-dom';
 import styled from "styled-components";
 import './Detail.scss'
 import { 재고context } from './App.js';
+import {Nav} from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
 
 let Box = styled.div`
     padding : 20px;
@@ -18,6 +20,8 @@ function Detail(props) {
   let [inputData, inputData변경] = useState(' ');
   let [alert, alert변경] = useState(false);
   let 재고 = useContext(재고context);
+  let [탭, 탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
 
     useEffect(() => {
         let timer = setTimeout(() => { alert변경(true) }, 5000)
@@ -65,8 +69,34 @@ function Detail(props) {
                 }}>뒤로가기</button> 
           </div>
         </div>
+
+        <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+          <Nav.Item>
+            <Nav.Link eventKey="link-0" onClick={()=>{ 스위치변경(false); 탭변경(0)}}>Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" onClick={()=>{ 스위치변경(false); 탭변경(1)}}>Option 2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <CSSTransition in={스위치} classNames="Anima" timeout={500}>
+          <TabContent 탭={탭} 스위치변경={스위치변경}/>
+        </CSSTransition>
       </div>
     )
+}
+
+function TabContent(props){
+
+  useEffect(()=>{
+    props.스위치변경(true);
+  });
+  
+  if (props.탭 === 0){
+    return <div> 0번째 내용 </div>
+  } else if (props.탭 === 1){
+    return <div> 1번째 내용 </div>
+  }
+
 }
 
 function Info(props){
@@ -74,4 +104,5 @@ function Info(props){
     <p>재고 : {props.재고[0]}</p>
   )
 }
+
 export default Detail;
